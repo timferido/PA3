9c0b797b7b6d2b480f7e9bb8e348a10053fb3163
Kent D Nguyen <kdn003@ieng6-202.ucsd.edu>
2017-11-08 14:55:32 -0800

    Documented BitOutput

--- BitOutputStream.cpp
+++ BitOutputStream.cpp
@@ -1,25 +1,44 @@
+/*
+ *                                                        Tim Ferido
+ *                                                        Kent Nguyen
+ *                                                        CSE 100, Fall 2017
+ *                                                        November 9, 2017
+ *                                                        A12880086 
+ *                                                        A12164917
+ *                        Programming Assignment Three
+ * File Name:    BitOutputStream.h
+ * Description:  Contains methods and data fields of the BitOutputStream class.
+ */ 
 #include "BitOutputStream.h"
 
+/** Initialize a BitOutputStream that will use 
+  * the given ostream for output */
 BitOutputStream::BitOutputStream(std::ostream & os) : out(os), buf(0), nbits(0) {}
 
+/** Send the buffer to the output, and clear it */
 void BitOutputStream::flush() {
     out.put(buf);
     out.flush();
     buf = nbits = 0;
 }
 
+/** Write the least significant bit of the argument to
+  * the bit buffer, and increment the bit buffer index.
+  * But flush the buffer first, if it is full. */
 void BitOutputStream::writeBit(int i) {
     
-    //if the bit is full 
+    //If the bit is full 
     if (nbits == 8) flush();
 
-    //write the least significant bit of i into buffer at current index
+    //Write the least significant bit of i into buffer at current index
     buf = setBit(buf, i, nbits);
 
-    //increment index
+    //Increment index
     nbits++;
 }
 
+/** Sets a bit's value of the nth bit from the right of a byte argument,
+  * and return the result. */
 byte BitOutputStream::setBit(byte b, int bit, int n) {
     return ((b&~(1<<n))|(bit<<n));
 }
--- BitOutputStream.h
+++ BitOutputStream.h
@@ -1,15 +1,36 @@
+/*
+ *                                                        Tim Ferido
+ *                                                        Kent Nguyen
+ *                                                        CSE 100, Fall 2017
+ *                                                        November 9, 2017
+ *                                                        A12880086 
+ *                                                        A12164917
+ *                        Programming Assignment Three
+ * File Name:    BitOutputStream.h
+ * Description:  Contains methods and data fields of the BitOutputStream class.
+ */ 
 #include <iostream>
-
-typedef unsigned char byte;
 
 class BitOutputStream {
 private:
-    char buf;
-    int nbits;
-    std::ostream & out;
+    char buf;               //One byte buffer of bits
+    int nbits;              //How many bits have been written to buf
+    std::ostream & out;     //Reference to the output stream
+    
 public:
+    /** Initialize a BitOutputStream that will use 
+      * the given ostream for output */
     BitOutputStream(std::ostream & os);
+    
+    /** Send the buffer to the output, and clear it */
     void flush();
-    void writeBit(int i);
-    byte setBit(byte b, int bit, int n);
+    
+    /** Write the least significant bit of the argument to
+      * the bit buffer, and increment the bit buffer index.
+      * But flush the buffer first, if it is full. */
+    void writeBit(int i)
+    
+    /** Sets a bit's value of the nth bit from the right of a byte argument,
+      * and return the result. */
+    void setBit(byte b, int bit, int n);
 };
